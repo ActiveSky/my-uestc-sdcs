6fd310d88bcd02bff55380d298d42d42b4d2b7fc
jikuixie <jikuixie@gmail.com>
2025-09-07 13:52:06 +0000

    test: remove obsolete test scripts

--- tests/test_rpc.py
+++
@@ -1,6 +0,0 @@
-import rpyc
-
-
-client = rpyc.connect('127.0.0.1', 5000)
-print(client.root.handle_rpc_request('set', 'tasks', ['a', 'b', 'c']))
-print(client.root.handle_rpc_request('get', 'tasks'))
